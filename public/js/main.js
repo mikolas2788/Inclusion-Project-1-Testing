4dc67213140c045d8bdb192e2f5e0b868731bc95
Myron <myron.yook@gmail.com>
2020-02-10 20:58:45 -0500

    work to reservation form

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,18 @@ const allReservations = () => {
 
 let reserveCalendar = new VanillaCalendar({
     selector: "#reserveCalendar",
-    pastDates: false 
+    pastDates: false,
+    onSelect: (data, ele) => {
+        let fixedDate = data.date.split(" ").slice(0,3).join(" ")
+        let selectedDate = document.getElementById("dateSelector")
+        selectedDate.value = fixedDate; 
+        console.log(data);
+    }
 })
+
+// function dateSelector(date) {
+//     let selectedDate = document.getElementsByName("dateSelector")
+//     selectedDate.value = date; 
+// } 
+
 // console.log(document.querySelector('p').innerHTML);
